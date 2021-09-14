548bdbf0be777a71c4eb988e48bf26f0cdc448d1
Andrew Vancil <72818699+andrew-vancil@users.noreply.github.com>
2021-09-14 13:36:40 -0400

    remove shinyapps from git

--- shinyapps.r
+++
@@ -1,7 +0,0 @@
-#Host on ShinyApps.io
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='andrew-vancil', 
-                          token='C5295018FACDBE10CB3895869327DB76', 
-                          secret='xywH6Nu2nVbIVRFHqixpdcPmJTe3v98uI7SRFK1l')
-rsconnect::deployApp(account = 'andrew-vancil')
